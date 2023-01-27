e51cab75e218c5e3638ce76c41791db1073d0156
asifrazaansari <asifraza0231@gmail.com>
2023-01-27 08:07:01 +0530

    frontend done

---
+++ src/pages/home/Home.js
@@ -0,0 +1,28 @@
+import Featured from '../../components/feautred/Featured'
+import Header from '../../components/header/Header'
+import Navbar from '../../components/navbar/Navbar'
+import PropertyList from '../../components/propertyList/PropertyList'
+import FeaturedProperties from '../../components/featuredProperties/FeaturedProperties'
+import './home.css'
+import MailList from '../../components/mailList/MaiList'
+import Footer from '../../components/footer/Footer'
+
+const Home = () => {
+  return (
+    <div>
+      <Navbar />
+      <Header />
+      <div className="homeContainer">
+        <Featured />
+        <h1 className="homeTitle">Browse by property type</h1>
+        <PropertyList />
+        <h1 className="homeTitle">Home guests love</h1>
+        <FeaturedProperties />
+        <MailList />
+        <Footer />
+      </div>
+    </div>
+  )
+}
+
+export default Home
